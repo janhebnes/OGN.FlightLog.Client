219bf5223bbd7a5fcc220b32d730e5211dd1e351
Jan Hebnes <jan.hebnes@gmail.com>
2015-11-14 20:29:46 +0100

    Removed singleton pattern from db context

--- OGN.FlightLog.Client/Client.cs
+++ OGN.FlightLog.Client/Client.cs
@@ -182,26 +182,30 @@
 
             // if request has records in database return results. 
             var datasetIdentifier = options.GetDatasetIdentifier();
-            var flights = FlightLogContext.Instance.Flights.Where(f => f.dataset == datasetIdentifier);
-            if (flights.Any())
+            using (var db = new FlightLogContext())
             {
-                if (flights.Any(f => f.row < 0)) // days with zero flights are stored with one flight with row -1;
-                    return new List<Flight>();
+                var flights = db.Flights.Where(f => f.dataset == datasetIdentifier);
+                if (flights.Any())
+                {
+                    if (flights.Any(f => f.row < 0)) // days with zero flights are stored with one flight with row -1;
+                        return new List<Flight>();
 
-                return flights.ToList();
+                    return flights.ToList();
+                }
+
+                // if request does not have records in database get directly from source and save results 
+                var result = GetLiveFlights(options);
+                if (!result.Any())
+                {
+                    // Store the day as having zero flights with an empty flight at row -1 (meaning we do not spam the webservice for zero flight days)
+                    result.Add(new Flight(options, -1));
+                }
+
+                db.Flights.AddRange(result);
+                db.SaveChanges();
+                return result;
             }
 
-            // if request does not have records in database get directly from source and save results 
-            var result = GetLiveFlights(options);
-            if (!result.Any())
-            {
-                // Store the day as having zero flights with an empty flight at row -1 (meaning we do not spam the webservice for zero flight days)
-                result.Add(new Flight(options, -1));
-            }
-
-            FlightLogContext.Instance.Flights.AddRange(result);
-            FlightLogContext.Instance.SaveChanges();
-            return result;
         }
 
 
--- OGN.FlightLog.Client/Models/FlightLogContext.cs
+++ OGN.FlightLog.Client/Models/FlightLogContext.cs
@@ -9,44 +9,11 @@ namespace OGN.FlightLog.Client.Models
 {
     public class FlightLogContext : DbContext
     {
-        private static FlightLogContext instance = null;
-        private static readonly object padlock = new object();
-
         public FlightLogContext() : base("OGN.FlightLog")
-        {
-        }
-
-        /// <summary>
-        /// Thread-Safe Singleton Instance
-        /// </summary>
-        public static FlightLogContext Instance
-        {
-            // TODO: Do we create a new Db Context for Ogn log data in owin or do we singleton ?
-            //  app.CreatePerOwinContext(ApplicationDbContext.Create);
-            //  context.Get<ApplicationDbContext>() that allows for a single instance to be used all over... 
-            get
-            {
-                lock (padlock)
-                {
-                    if (instance == null)
-                    {
-                        instance = new FlightLogContext();
-                    }
-                    return instance;
-                }
-            }
-        }
-
-        static FlightLogContext()
         {
             Database.SetInitializer<FlightLogContext>(new MigrateDatabaseToLatestVersion<FlightLogContext, Migrations.FlightLogContext.Configuration>());
         }
-
-        public static FlightLogContext Create()
-        {
-            return new FlightLogContext();
-        }
-
+        
         public DbSet<Flight> Flights { get; set; }
     }
 
