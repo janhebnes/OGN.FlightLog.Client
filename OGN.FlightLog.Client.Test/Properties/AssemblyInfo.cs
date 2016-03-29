78f3fefb1a850871c1ac531d527baf0a3cfa46f1
Jan Hebnes <jan.hebnes@gmail.com>
2016-03-29 21:54:27 +0200

    updated assembly info

--- OGN.FlightLog.Client.Test/Properties/AssemblyInfo.cs
+++ OGN.FlightLog.Client.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OGN-FlightLog-Dataminer.Test")]
+[assembly: AssemblyTitle("OGN.FlightLog.Client.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OGN-FlightLog-Dataminer.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("OGN.FlightLog.Client.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- OGN.FlightLog.Client/Properties/AssemblyInfo.cs
+++ OGN.FlightLog.Client/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OGN-FlightLog-Monitor")]
+[assembly: AssemblyTitle("OGN.FlightLog.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OGN-FlightLog-Monitor")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("OGN.FlightLog.Client")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
